cc5891e2990d01e8862ce184d2eae41f8eb03857
Alex-Kinuthia <alexkinuthia125@gmail.com>
2017-05-29 10:24:37 +0300

    modified files and added a new java class "Constants.java"

---
+++ app/src/main/java/com/example/alex/myrestaurant/Constants.java
@@ -0,0 +1,11 @@
+package com.example.alex.myrestaurant;
+
+/**
+ * Created by alex on 5/29/17.
+ */
+
+public class Constants {
+    public static final String YELP_CLIENT_ID = BuildConfig.YELP_CLIENT_ID;
+    public static final String YELP_CLIENT_SECRET = BuildConfig.YELP_CLIENT_SECRET;
+    public static final String YELP_ACCESS_TOKEN = BuildConfig.YELP_ACCESS_TOKEN;
+}
