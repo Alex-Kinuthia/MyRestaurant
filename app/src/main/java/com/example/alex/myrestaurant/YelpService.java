df0baecacd81f1719518edb592fde485f4082e51
Alex-Kinuthia <alexkinuthia125@gmail.com>
2017-05-29 12:37:34 +0300

    modified files

--- app/src/main/java/com/example/alex/myrestaurant/YelpService.java
+++ app/src/main/java/com/example/alex/myrestaurant/YelpService.java
@@ -3,6 +3,7 @@ package com.example.alex.myrestaurant;
 /**
  * Created by alex on 5/29/17.
  */
+import okhttp3.Call;
 import okhttp3.Callback;
 import okhttp3.HttpUrl;
 import okhttp3.OkHttpClient;
@@ -13,21 +14,20 @@ import se.akerfeldt.okhttp.signpost.SigningInterceptor;
 
 
 public class YelpService {
+    private static OkHttpClient client = new OkHttpClient();
+
     public static void findRestaurants(String location, Callback callback) {
-        OkHttpOAuthConsumer consumer = new OkHttpOAuthConsumer(Constants.YELP_CONSUMER_KEY, Constants.YELP_CONSUMER_SECRET);
-        consumer.setTokenWithSecret(Constants.YELP_TOKEN, null);
-
-        OkHttpClient client = new OkHttpClient.Builder()
-                .addInterceptor(new SigningInterceptor(consumer))
-                .build();
-
         HttpUrl.Builder urlBuilder = HttpUrl.parse(Constants.YELP_BASE_URL).newBuilder();
         urlBuilder.addQueryParameter(Constants.YELP_LOCATION_QUERY_PARAMETER, location);
         String url = urlBuilder.build().toString();
 
-        Request request= new Request.Builder()
+        Request request = new Request.Builder()
+                .header("Authorization", "Bearer " + Constants.YELP_TOKEN)
                 .url(url)
                 .build();
+
+        Call call = client.newCall(request);
+        call.enqueue(callback);
     }
 }
 
