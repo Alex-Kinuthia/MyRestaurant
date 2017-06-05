b80ba15cf8cda5b0110a17b02e3c5216b7bc49b4
Alex-Kinuthia <alexkinuthia125@gmail.com>
2017-06-05 12:44:16 +0300

    changes in the MainActivity.java First, we begin by creating member variables to store reference to the shared preferences tool itself  (mSharedPreferences) and the dedicated tool we must use to edit them (mEditor). Next, we create a method called addToSharedPreferences(), which takes the user-inputted zip code as an argument addToSharedPreferences() calls upon the editor to write this information to shared preferences. The, since shared preference data must be in key-value pairs,  we provide the editor the key we've stored in our Constants file called PREFERENCES_LOCATION_KEY, and the zip code value we've passed in as an argument, location. Finally, we call apply() to save this information.

--- app/src/main/java/com/example/alex/myrestaurant/ui/MainActivity.java
+++ app/src/main/java/com/example/alex/myrestaurant/ui/MainActivity.java
@@ -1,7 +1,9 @@
 package com.example.alex.myrestaurant.ui;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.graphics.Typeface;
+import android.preference.PreferenceManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -9,6 +11,7 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 
+import com.example.alex.myrestaurant.Constants;
 import com.example.alex.myrestaurant.R;
 
 import butterknife.Bind;
@@ -16,9 +19,13 @@ import butterknife.ButterKnife;
 
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    private SharedPreferences mSharedPreferences;
+    private SharedPreferences.Editor mEditor;
+
     @Bind(R.id.findRestaurantsButton) Button mFindRestaurantsButton;
     @Bind(R.id.locationEditText) EditText mLocationEditText;
-    @Bind(R.id.appNameTextView) TextView mAppNameTextView;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,8 +33,8 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         setContentView(R.layout.activity_main);
         ButterKnife.bind(this);
 
-        Typeface ostrichFont = Typeface.createFromAsset(getAssets(), "fonts/ostrich-regular.ttf");
-        mAppNameTextView.setTypeface(ostrichFont);
+        mSharedPreferences = PreferenceManager.getDefaultSharedPreferences(this);
+        mEditor = mSharedPreferences.edit();
 
         mFindRestaurantsButton.setOnClickListener(this);
     }
@@ -36,9 +43,13 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     public void onClick(View v) {
         if(v == mFindRestaurantsButton) {
             String location = mLocationEditText.getText().toString();
+            addToSharedPreferences(location);
             Intent intent = new Intent(MainActivity.this, RestaurantListActivity.class);
-            intent.putExtra("location", location);
             startActivity(intent);
         }
+    }
+
+    private void addToSharedPreferences(String location) {
+        mEditor.putString(Constants.PREFERENCES_LOCATION_KEY, location).apply();
     }
 }
