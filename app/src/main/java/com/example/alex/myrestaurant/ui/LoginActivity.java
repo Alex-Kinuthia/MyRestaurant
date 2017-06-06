3dbd79afc77c640df90ed665c2d567e426c6528f
Alex-Kinuthia <alexkinuthia125@gmail.com>
2017-06-06 10:49:54 +0300

    creation of user authentification setup

---
+++ app/src/main/java/com/example/alex/myrestaurant/ui/LoginActivity.java
@@ -0,0 +1,15 @@
+package com.example.alex.myrestaurant.ui;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.alex.myrestaurants.R;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+}
